442cba355341e085111652cbe77bbd43eb4c2ec7
沈佳佳 <jiajia@victor1107demacbook-pro.local>
2016-04-28 15:49:35 +0800

    Initial Commit

---
+++ 我们的旅行/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我们的旅行
+//
+//  Created by 佳佳 on 16/4/28.
+//  Copyright © 2016年 佳佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 我们的旅行/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  我们的旅行
+//
+//  Created by 佳佳 on 16/4/28.
+//  Copyright © 2016年 佳佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
