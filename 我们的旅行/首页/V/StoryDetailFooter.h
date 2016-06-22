0dd2bc0e150b0a49f0ec732acdfe4e97cb2da6ac
jiajia211 <564042134@qq.com>
2016-06-22 16:52:38 +0800

    详情footer

---
+++ 我们的旅行/首页/V/StoryDetailFooter.h
@@ -0,0 +1,13 @@
+//
+//  StoryDetailFooter.h
+//  我们的旅行
+//
+//  Created by 佳佳 on 16/6/22.
+//  Copyright © 2016年 佳佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryDetailFooter : UIView
+
+@end
---
+++ 我们的旅行/首页/V/StoryDetailFooter.m
@@ -0,0 +1,20 @@
+//
+//  StoryDetailFooter.m
+//  我们的旅行
+//
+//  Created by 佳佳 on 16/6/22.
+//  Copyright © 2016年 佳佳. All rights reserved.
+//
+
+#import "StoryDetailFooter.h"
+
+@interface StoryDetailFooter()
+@property (weak, nonatomic) IBOutlet UILabel *storyTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *storyTimeLabel;
+
+@end
+@implementation StoryDetailFooter
+
+
+
+@end
